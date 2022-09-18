f714ed823a7cd231dd2168346e75e175cc847dfa
yankvasya <yankvasya@gmail.com>
2022-09-18 17:55:10 +0300

    feat: added `POST` request for `/mail`

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 import { SMTPClient } from 'emailjs';
+import express from 'express'
 import * as dotenv from "dotenv";
+import bodyParser from 'body-parser'
 dotenv.config();
 
-const client = new SMTPClient({
-    user: process.env.EMAIL,
-    password: process.env.PASS,
-    host: 'smtp.gmail.com',
-    ssl: true,
+const app = express();
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', urlencodedParser, function (req, res) {
+    res.send('Hello World!');
 });
 
-try {
-    const message = await client.sendAsync({
-        text: 'form text',
-        from: `form <${process.env.USER}>`,
-        to: `you <${process.env.TO}>`,
-        subject: 'Отправка формы',
+app.post('/mail', jsonParser, async (req, res) => {
+    console.log(req.body)
+    const { email, phone, name } = req.body;
+    const text =
+        (process.env.MAIL_TITLE || "Сообщение с формы") +
+        "\n" +
+        `Имя: ${name}\n` +
+        `Почта: ${email}\n` +
+        `Телефон: ${phone}`;
+
+
+    const client = new SMTPClient({
+        user: process.env.EMAIL,
+        password: process.env.PASS,
+        host: 'smtp.gmail.com',
+        ssl: true,
     });
-    console.log(message);
-} catch (err) {
-    console.error(err);
-}
+
+    try {
+        const message = await client.sendAsync({
+            text: text,
+            from: `form <${process.env.USER}>`,
+            to: `you <${process.env.TO}>`,
+            subject: 'Отправка формы',
+        });
+        console.log(message);
+        res.send('Сообщение успешно отправлено')
+    } catch (err) {
+        console.error(err);
+        res.send('Произошла ошибка при отправки сообщения')
+    }
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
